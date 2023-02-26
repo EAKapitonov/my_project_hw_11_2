f249cf73a3ea27199415dc89db875f4dfd9fc0fd
EAKapitonov <Kapitonov281087@mail.tu>
2023-02-26 23:30:00 +0900

    add word_up_first(words), add docstring, chek conflict

--- str_func.py
+++ str_func.py
@@ -3,6 +3,6 @@ def word(words):
     word_ = words.upper()
     return word_
 def word_up_first(words):
-    """Меняет первую букву на заглавную"""
+    """Меняет первую букву на заглавную!"""
     word_ = words.title()
     return word_
