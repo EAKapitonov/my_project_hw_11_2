319c90b23fdea9d155c9cd69a559d29fe3bc0db8
EAKapitonov <Kapitonov281087@mail.tu>
2023-02-26 23:04:56 +0900

    add .gitignore

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
 def word(words):
+    """Меняет все буквы на заглавные"""
     word_ = words.upper()
+    return word_
+def word_up_first(words):
+    """Меняет первую букву на заглавную"""
+    word_ = words.title()
     return word_
